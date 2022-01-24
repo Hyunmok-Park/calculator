61221077dd69735ab8d14db587014678febfc925
River <river@nhnedu.com>
2022-01-24 17:17:20 +0900

    add function implemented

--- main.c
+++ main.c
@@ -36,8 +36,8 @@ int main(int argc, char* argv[])
  
 int add(int number1, int number2)
 {
-	//TODO implementation
-	return 0;
+
+	return number1 + number2;
 }
  
 int sub(int number1, int number2)
@@ -57,3 +57,4 @@ int div(int number1, int number2)
 	//TODO implementation
 	return 0;
 }
+
